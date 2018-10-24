0ff1905d90c2db1fa8806906a768688bab18c844
R.G.Esteves <rgesteve@gmail.com>
2018-10-24 03:36:22 +0000

    Serving static content

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 let express = require('express');
+let path = require('path');
 
 const SERVERPORT = 8080;
 
 let app = express();
-app.get('/', (req, res) => {
-    res.send("Hello, world!");
+app.use(express.static(path.join(__dirname, '/')));
+
+app.get((req, res) => {
+    res.writeHead(200, {"Content-Type" : "text/plain"});
+    res.end("Looks like you didn't find a file we have");
 });
 
 app.listen(SERVERPORT, () => {
